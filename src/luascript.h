c666e218ae39c4e94cac24091ee39a99259907a2
HeavenIsLost <yamaken93@outlook.com>
2014-09-28 17:40:58 -0300

    * Player Rates( now you can control and replace config.lua experience and skills rates for every player )

--- src/luascript.h
+++ src/luascript.h
@@ -978,6 +978,9 @@ class LuaScriptInterface
 		static int32_t luaPlayerGetContainerById(lua_State* L);
 		static int32_t luaPlayerGetContainerIndex(lua_State* L);
 
+		static int32_t luaPlayerGetRate(lua_State* L);
+		static int32_t luaPlayerSetRate(lua_State* L);
+
 		// Monster
 		static int32_t luaMonsterCreate(lua_State* L);
 
